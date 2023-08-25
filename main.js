da406af987d52967da4ddb036941c9e99c0c65d8
Vivek <scuti.bgstar@gmail.com>
2023-08-25 19:42:53 +0530

    bhaji pala solved

---
+++ main.js
@@ -0,0 +1,16 @@
+function lenisSmoothScroll(){
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+}
+
+lenisSmoothScroll();
